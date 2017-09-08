55101e52f3a1245b600cf4cb9acc64dd0ec1acd4
Nikolai Vorobiev <nikolai.vorobiev@gmail.com>
2017-09-08 14:58:02 -0400

    Changes made to Person4.java

---
+++ readme.p1.txt
@@ -0,0 +1,6 @@
+Austin Ayers
+aayers31@gatech.edu
+
+Brodents are the best
+
+Destiny 2 eats my time
--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,8 +3,8 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
@@ -31,15 +31,21 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+      String rotated = "";
+      for (int i = 2; i <= input.length() - 1; i--) {
+          rotated = rotated + input.charAt(i);
+      }
+      rotated = rotated + input.charAt(0);
+      rotated = rotated + input.charAt(1);
+      return rotated;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
