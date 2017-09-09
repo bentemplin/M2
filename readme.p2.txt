a191f5b15283ff70c91602838d3c79d1dfa089f9
Rikesh Subedi <rsubedi3@gatech.edu>
2017-09-08 22:49:41 -0400

    Implemented all changes for P2:
    - Remove useless text file.
    - Implemented functions for P2.
    - Created readme file for P2.

---
+++ readme.p2.txt
@@ -0,0 +1,4 @@
+Person 2: Rikesh Subedi.
+Email: rsubedi3@gatech.edu
+Favorite Band: Future Islands (right now)
+Hello World!
--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,9 +1,12 @@
 package edu.gatech.oad.antlab.person;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
  * @version 1.1
@@ -11,7 +14,7 @@ package edu.gatech.oad.antlab.person;
 public class Person2 {
     /** Holds the persons real name */
     private String name;
-	 	/**
+	 /**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
@@ -19,6 +22,7 @@ public class Person2 {
 	 public Person2(String pname) {
 	   name = pname;
 	 }
+
 	/**
 	 * This method should take the string
 	 * input and return its characters in
@@ -31,14 +35,27 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+      ArrayList<Character> inAsList = new ArrayList<Character>();
+      String jumStr = new String();
+
+      for (char c: input.toCharArray()) {
+          inAsList.add(c);
+      }
+
+      Collections.shuffle(inAsList);
+
+      for (char c: inAsList) {
+          jumStr += c;
+      }
+
+	  return jumStr;
 	}
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
